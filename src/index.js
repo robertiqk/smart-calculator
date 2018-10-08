ccc98dbdab98b99ddd4a34230070d2de3e75af7e
robertiqk <delux1945@gmail.com>
2018-10-08 18:25:40 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
-  }
+    this.initialValue = initialValue + "";
 
+  }
+  
   add(number) {
     // your implementation
+    this.initialValue += "+" + number;
+    return this;
   }
   
   subtract(number) {
     // your implementation
+    this.initialValue += "-" + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.initialValue += "*" + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.initialValue += "/" + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.initialValue += "**" + number;
+    return this;
+  }
+
+  valueOf() {
+  return this.initialValue = eval(this.initialValue);
   }
 }
 
